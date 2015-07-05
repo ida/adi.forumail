d56c447b61e0780756af42617bdc78eac6ce82a1
Ida Ebkes <contact@ida-ebkes.eu>
2015-07-05 19:35:40 +0200

    up

--- build_forumail.sh
+++ build_forumail.sh
@@ -1,20 +1,26 @@
-# Set/change the following 4 vars!
+# Set the following 4 vars:
 account_name=yourMailAccountName[@yourMailProvider.com]
 account_password=yourSuperSecretPassword
 imap_server=imap.yourMailProvider.com
 imap_port=993
 
 ############# Don't change the following, unless, you know. ###############
-
 dropbox=http://admin:admin@localhost:8080/Plone/dropbox
 skript=src/mailtoplone.base/mailtoplone/base/scripts/fetchemail
+eggs_dir=$HOME/.buildout/eggs
 
+# Create egss-dir:
+mkdir -p $eggs_dir
 # Create a virtualenviroment for an isolated Python-env:
-mkdir -p $HOME/.buildout/eggs
-virtualenv $HOME/.virtenv
+virtualenv --no-site-packages $HOME/.virtenv
+# Activate the enviroment:
+. $HOME/.virtenv/bin/activate
+
+# Upgrade setuptools, overcoming potential vs-conflicts:
+$HOME/.virtenv/bin/pip install setuptools -U
 
 # Install buildout in the virtenv:
-./$HOME/.virtenv/bin/pip install zc.buildout
+$HOME/.virtenv/bin/pip install zc.buildout
 
 # Create folder for instance and dev-eggs:
 mkdir -p instance/src
@@ -42,13 +48,13 @@ parts =
 " >> buildout.cfg
 
 # Add an entry for every cron:
-for i in {0..5}
+for i in {1..6}
 do
     printf "    mailtoplone0${i}of06\n" >> buildout.cfg
 done
 
 # Add next static string to buildout.cfg:
-printf "eggs-directory = $HOME/.buildout/eggs
+printf "\neggs-directory = $eggs_dir
 extends = http://dist.plone.org/release/4.3.5/versions.cfg
 develop =
     src/adi.forumail
@@ -76,18 +82,23 @@ zc.buildout =
 kommand="${skript} -u ${dropbox} -i ${imap_server} -t ${imap_port} -e ${account_name} -p ${account_password}"
 for i in {0..5}
 do
-    printf "\n[mailtoplone0${i}of06]
-# Fetch mails of inbox every minute on the ${i}0th second:
+    ((i++))
+    printf "\n[mailtoplone0${i}of06]" >> buildout.cfg
+    ((i--))
+    printf "\n# Fetch mails of inbox every minute on the ${i}0th second:
 recipe = z3c.recipe.usercrontab
 times = * * * * *
-command = sleep ${i}0 ${kommand}
-" >> buildout.cfg
+command = sleep ${i}0 ${kommand}\n" >> buildout.cfg
 done
 
 
-########################
-#        MAIN          #
-########################
-buildout # execute buildout
-./bin/instance fg#start # start ZOPE-server
-#firefox localhost:8080/Plone/dropbox # open browser and watch the plonesite
+###############################
+#      Execute buildout       #
+###############################
+buildout
+
+###############################
+#      Start server           #
+###############################
+./bin/instance fg
+
