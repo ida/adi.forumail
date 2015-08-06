03b97966b2331aac97920ecb8c7d011c21d1a095
Ida Ebkes <contact@ida-ebkes.eu>
2015-08-06 04:57:26 +0200

    Toggle quotations

--- adi/forumail/skins/adi_forumail/adi_forumail.js
+++ adi/forumail/skins/adi_forumail/adi_forumail.js
@@ -1,21 +1,68 @@
 (function($) { $(document).ready(function() {
-//$("#visual-portal-wrapper").prepend("Congrats, adi_forumail.js has been loaded succesfully!")
+function setRepliesIndent(posts) {
+    $(posts).each(function() {
+        var post = $(this)
+        var post_title = post.find('.summary')
+        var text = post_title.text()
+        var indent = 0
+        while( text.startsWith('Re: ') ) {
+            indent += 1
+            text = text.substring(4, text.length)
+        }
+        post.css('margin-left', String(indent) + 'em')
+    });
+}
+function wrapMailQuotations(posts) {
+    // Wrap quotations into a div and hide it.
+    $(posts).each(function() {
+        var post = $(this)
+        var post_body = post.find('div p')
+        var post_message = post_body.html()
+        var lines = post_message.split('<br>')
+        var digest = ''
 
-
-// Add indent to post, if starting with 'Re: ':
-$('.summary').each(function() {
-    var indent = 0
-    var text = $(this).text()
-    // Add one indent per 'Re: ':
-    while( text.startsWith('Re: ') ) {
-        indent += 1
-        text = text.substring(4, text.length)
+        // Collect quots start and end posis:
+        var i = -1
+        while(i<lines.length-1) {
+            i += 1
+            var line = lines[i]
+            if(line.startsWith('&gt;')){
+                digest += '<span class="toggle">Hide</span><div class="quotation">'
+                while(line.startsWith('&gt;') && i<lines.length-1){
+                    i += 1
+                    line = lines[i]
+                    digest += line + '<br>'
+                }
+                digest += '</div>'
+            }
+            digest += line + '<br>'
+        }
+    post_body.html(digest)
+    }); // each post
+}
+function toggleQuotVisi(button) {
+    var post = button.parent()
+    post.find('div div').toggle()
+    if(button.text() == 'Hide'){
+        button.text('Show')    
     }
-    // Set indent:
-    $(this).parent().parent().css('margin-left', String(indent) + 'em')
-}); // each
-
-
-
-
-}); })(jQuery);
+    else {
+        button.text('Hide')    
+    }
+}
+function main() {
+    var posts = $('#content-core .item')
+    setRepliesIndent(posts)
+    wrapMailQuotations(posts)
+    $('#content-core .toggle').click(function(){
+        $(this).find('~ *').toggle()
+        if($(this).text()=='Hide'){
+            $(this).text('Show')        
+        }
+        else {
+            $(this).text('Hide')
+        }
+    });
+    $('#content-core .toggle').click() // ini hide
+}
+main() }); })(jQuery);
