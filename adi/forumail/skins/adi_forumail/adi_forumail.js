1cb291e4ded5f8fb79114a3cf0bdaf4ecfa2b188
Ida Ebkes <contact@ida-ebkes.eu>
2015-08-19 08:36:31 +0200

    Indent replies. Fixes #11

--- adi/forumail/skins/adi_forumail/adi_forumail.js
+++ adi/forumail/skins/adi_forumail/adi_forumail.js
@@ -1,23 +1,52 @@
 (function($) { $(document).ready(function() {
+function isNr(chara) {
+    IS_NR = '0'
+    var nrs = ['1','2','3','4','5','6','7','8','9','0']
+    for(var i=0;i<nrs.length;i++) {
+        if(chara == nrs[i]) {
+            IS_NR = '1'
+        }
+    }
+    return IS_NR
+}
+function indentReply(post_link) {
+    var url = post_link.attr('href')
+    var indent_depth = 0
+    var i = url.length-1
+    var chara = url[i]
+    // Get indent-depth:
+    while(i > -1 && isNr(chara) == '1' || chara == '-') {
+        if(chara == '-') {
+            indent_depth += 1
+        }
+        i -= 1
+        chara = url[i]
+    } // while is nr or minus
+    // Set indent:
+    if(indent_depth > 0) {
+        post_link.parent().parent().css('margin-left', indent_depth + 'em')
+    }
+}
+function adjustReplyToPath(reply_to_link) {
+    // If in collection-view, prepend dot to reply-to-links,
+    // so reply will be created in container, not in collection:
+    reply_to_link.attr('href', '.' + reply_to_link.attr('href'))
+}
+function setTitleOfUrlHash() {
+    var title = window.location.href.split('#')[1]
+    $('#title').val(title)
+    $('#title').parent().hide() // Hide field, so user cannot damage auto-title.
+}
 function main() {
 
-    // If in collection-view, prepend dot to reply-to-links,
-    // so reply will be created in container not in collection:
+    // Listview:
     if($('.section-forumail.portaltype-topic').length > 0) {
-        $('.item #portlets-below a').each(function() {
-            $(this).attr('href', '.' + $(this).attr('href'))
-        }); // each reply-to-link
+        $('.item #portlets-below a').each(function() { adjustReplyToPath($(this)) });
+        $('.headline a').each(function() { indentReply($(this)) });
     }
 
-    // If in edit-mode and url contains a hash ...
+    // Editview and is a reply, because url contains a hash:
     if($('.section-forumail.template-atct_edit').length > 0 && window.location.href.indexOf('#') != -1) {
-        // ... we are in a reply and set the title to be anything following after url-hash:
-        var title = window.location.href.split('#')[1]
-        $('#title').val(title)
-        $('#text').val('<a href="#">a</a>')
-        $('#text_text_format').focus()
-        $('#title').parent().hide()
+        setTitleOfUrlHash()
     }
-/////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////
 } /* EO main */ main() }); /* EO doc.ready */ })(jQuery);
