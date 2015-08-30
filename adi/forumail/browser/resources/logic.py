295c5b6892366f10bd8668ce2707b1d52903096b
Ida Ebkes <contact@ida-ebkes.eu>
2015-08-30 11:43:17 +0200

    Savepoint sorting.

--- adi/forumail/browser/resources/logic.py
+++ adi/forumail/browser/resources/logic.py
@@ -17,30 +17,6 @@ class View(BrowserView):
     def __call__(self):
         return self.render()
 
-    def isNr(string):
-        IS_NR = False
-        nrs = ['0','1','2','3','4','5','6','7','8','9']
-        i = len(string)-1
-        while i < len(nrs)-1 and string[i] in nrs:
-            i -= 1
-        if i == 0: IS_NR = True
-        return IS_NR
-
-
-    def getThreadId(self, post_id):
-        thread_id = None
-        nrs = ['0','1','2','3','4','5','6','7','8','9']
-        i = len(post_id)-1
-        if post_id[-1] in nrs:
-            while i > 0 and post_id[i] in nrs or post_id[i] == '-':
-                i -= 1
-                if post_id[i] == '-' and not post_id[i-1] in nrs:
-                    break
-            thread_id = post_id[:i]
-        else:
-            thread_id = post_id
-        return thread_id
-
     def getPosts(self):
         """
         Expects forum-(Folder)- or post-(News Item)-object,
@@ -69,21 +45,75 @@ class View(BrowserView):
                     posts.append(post)
         return posts
 
-    def getPostIds(self):
+    def getThreadId(self, post_id):
+        thread_id = None
+        nrs = ['0','1','2','3','4','5','6','7','8','9']
+        i = len(post_id)-1
+        if post_id[-1] in nrs:
+            while i > 0 and post_id[i] in nrs or post_id[i] == '-':
+                i -= 1
+                if post_id[i] == '-' and not post_id[i-1] in nrs:
+                    break
+            thread_id = post_id[:i]
+        else:
+            thread_id = post_id
+        return thread_id
+
+    def getPostsIds(self):
         post_ids = []
         posts = self.getPosts()
         for post in posts:
-            post_ids.append(post.getId())
+            post_id = post['id']
+            post_ids.append(post_id)
         return post_ids
 
-    def getNextId(self, post_id):
-        """ Expects a post-obj and post-id, returns next reply-to-id."""
-        post_id += '-1'
-        post_ids = self.getPostIds()
-        i = 1
-        while post_id in post_ids:
-            i += 1
-            post_id = '-'.join(post_id.split('-')[:-1])
-            post_id += '-' + str(i)
-        return post_id
+    def getThreadsIds(self):
+        threads_ids = []
+        posts_ids = self.getPostsIds()
+        for post_id in posts_ids:
+            thread_id = self.getThreadId(post_id)
+            if thread_id not in threads_ids:
+                threads_ids.append(thread_id)
+        return threads_ids
+
+    def isReply(self, post_id, thread_id):
+        IS_REPLY = False
+        nrs = ['1','2','3','4','5','6','7','8','9','0']
+        if len(post_id) >= len(thread_id) + 1 \
+        and post_id.startswith(thread_id + '-')\
+        and post_id[len(thread_id) + 1] in nrs:
+            IS_REPLY =  True
+        return IS_REPLY 
+
+    def getThreads(self):
+        """
+        Sorted by newest ini-post.
+        """
+        threads = []
+        thread = None
+        threads_ids = self.getThreadsIds()
+        posts = self.getPosts()
+
+        for thread_id in threads_ids:
+            
+            i = -1
+            while i < len(posts)-1:
+                i += 1
+            
+                post = posts[i]
+                post_id = post['id']
+                
+                if post_id == thread_id:
+                    threads.append(posts.pop(i))
+            
+                    i = -1
+                    while i < len(posts)-1:
+                        i += 1
+            
+                        post = posts[i]
+                        post_id = post['id']
+            
+                        if post_id.startswith(thread_id):
+                            threads.append(posts.pop(i))
+        return threads
 
